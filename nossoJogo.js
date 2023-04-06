f45f0eaa92b6386807020000b15a5c59f63e0b96
Jonatha-Santos <jonathansantos_@hotmail.com>
2023-04-06 13:32:56 -0300

    Realização do projeto BlackJack

--- nossoJogo.js
+++ nossoJogo.js
@@ -1,12 +1,43 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+
+                                      //! PROJETO BLACKJACK !\\         
+                                       //! CÓDIGO ABAIXO 👇🏻 !\\
+
+
+console.log ("Boas vindas ao jogo de BlackJack")
+
+if (confirm("Deseja jogar uma partida? \n'OK' para SIM e 'CANCEL' para NÃO")) {
+
+   let isOk = false
+   let userCard = []
+   let computerCard = []
+
+   while (!isOk) {
+      userCard.push (comprarCarta())
+      userCard.push (comprarCarta())
+      computerCard.push (comprarCarta())
+      computerCard.push (comprarCarta())
+      if (userCard[0].valor === 11 && userCard[1].valor === 11 || computerCard[0].valor === 11 && computerCard[1].valor === 11) {
+         userCard = []
+         computerCard = []
+      } else {
+         isOk = true
+      }
+   } 
+
+   let userScore = userCard[0].valor + userCard[1].valor
+   let computerScore = computerCard[0].valor + computerCard[1].valor
+
+   console.log(`Usuário: ${userCard[0].texto}, ${userCard[1].texto}  = ${userScore} `)
+   console.log(`Computador: ${computerCard[0].texto}, ${computerCard[1].texto} = ${computerScore}`)
+   
+   if (userScore > computerScore){
+      console.log("Parabéns, você venceu!")
+   } else if (computerScore > userScore) {
+      console.log("O computador venceu, que pena!")
+   } else if (userScore === computerScore) {
+      console.log("A rodada terminou em empate!")
+   }
+ } else { 
+   console.log("Obrigado pela presença, volte outra vez caso deseje jogar BlackJack!")
+ } 
+
